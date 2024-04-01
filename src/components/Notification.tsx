f81c8976c4d9697991ed27471ed2fb90dceef31b
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-03-31 21:30:35 -0400

    Update Notification.tsx

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Notifcation = () => {
-  return <div>Notifcation</div>
+  return (
+    <div className='h-12 bg-red-500 text-white'>
+      Free delivery for all orders over $50. Order your cooking essentials now!
+    </div>
+  )
 }
 
 export default Notifcation
