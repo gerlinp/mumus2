5173e83c1a81c3366e0b7e4150c7bcb41ed903d3
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-04-10 23:53:59 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Menu from './Menu'
 import Link from 'next/link'
 import Image from 'next/image'
+import CartIcon from './CartIcon'
 
 const user = false
 const Navbar = () => {
@@ -33,7 +34,7 @@ const Navbar = () => {
         ) : (
           <Link href='/orders'>Orders</Link>
         )}{' '}
-        <Link href='/'>CartIcon</Link>
+        <CartIcon/>
       </div>
     </div>
   )
