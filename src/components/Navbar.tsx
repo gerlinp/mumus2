2f2fe60538efe4c8959f5cc55c6c909c565cde6e
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-04-11 23:21:13 -0400

    correction

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,30 +12,31 @@ const Navbar = () => {
       <div>
         <Link href='/'>Mumu's Pikliz</Link>
       </div>
-
       <div className='hidden md:flex gap-4'>
         <Link href='/'>Homepage</Link>
         <Link href='/'>Shop</Link>
         <Link href='/'>Locations</Link>
         <Link href='/'>Contact</Link>
       </div>
-
       {/* mobile menu */}
       <div className='flex md:hidden'>
         <Menu />
       </div>
       {/* Right Links */}
       <div className='hidden md:flex gap-4' items-center>
-        <div className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1' rounded-md/>
+        <div
+          className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1'
+          rounded-md
+        >
           <Image src='phone.png' alt='phone' width={20} height={20} />
         </div>
-        {!user ? (
-          <Link href='/login'>Login</Link>
-        ) : (
-          <Link href='/orders'>Orders</Link>
-        )}{' '}
-        <CartIcon/>
       </div>
+      {!user ? (
+        <Link href='/login'>Login</Link>
+      ) : (
+        <Link href='/orders'>Orders</Link>
+      )}{' '}
+      <CartIcon />
     </div>
   )
 }
