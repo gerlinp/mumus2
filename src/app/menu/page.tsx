588fb8cc346e403d834f09548af864e8a7ad08a3
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-05-01 23:58:28 -0400

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
+import { menu } from '@/data'
 
 const Menu = () => {
   return
   ;<div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
-    MenuPage
+    {menu.map((category) => (
+      <Link href={category.slug} key={category.id} className=''>
+        <h1>{category.title}</h1>
+        <p>{category.desc}</p>
+        <button className=''>Explore</button>
+      </Link>
+    ))}
   </div>
 }
 
