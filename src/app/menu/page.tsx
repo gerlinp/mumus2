0c0f4c76a305cac58de8408d6365fbf3a390f934
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-05-02 23:30:46 -0400

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,7 +6,12 @@ const Menu = () => {
   return
   ;<div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
     {menu.map((category) => (
-      <Link href={category.slug} key={category.id} className=''>
+      <Link
+        href={category.slug}
+        key={category.id}
+        className='w-full h1/3 bg-cover'
+        style={{ backgroundImage: 'url($category.img)' }}
+      >
         <h1>{category.title}</h1>
         <p>{category.desc}</p>
         <button className=''>Explore</button>
