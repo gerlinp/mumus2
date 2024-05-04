46e2f395f0493dcbb9f7c20bfa677e56b1d7adab
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-05-03 23:52:39 -0400

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,20 +4,24 @@ import { menu } from '@/data'
 
 const Menu = () => {
   return
-  ;<div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
-    {menu.map((category) => (
-      <Link
-        href={category.slug}
-        key={category.id}
-        className='w-full h1/3 bg-cover'
-        style={{ backgroundImage: 'url($category.img)' }}
-      >
-        <h1>{category.title}</h1>
-        <p>{category.desc}</p>
-        <button className=''>Explore</button>
-      </Link>
-    ))}
-  </div>
+  ;<>
+    <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
+      {menu.map((category) => (
+        <Link
+          href={'/menu/$category.slug'}
+          key={category.id}
+          className='w-full h1/3 bg-cover p-8'
+          style={{ backgroundImage: 'url($category.img)' }}
+        >
+          <div className={'text-${category.color} w-1/2'}>
+            <h1 className='uppercase font-bold text-3xl'>{category.title}</h1>
+            <p className='text-sm my-8'>{category.desc}</p>
+            <button className={'hidden 2xl:block'}>Explore</button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  </>
 }
 
 export default Menu
