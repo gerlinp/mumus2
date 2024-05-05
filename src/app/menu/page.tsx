9e45e1ca4d25e1db56979bcffdf27908ef000a9b
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-05-04 23:57:43 -0400

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -10,13 +10,19 @@ const Menu = () => {
         <Link
           href={'/menu/$category.slug'}
           key={category.id}
-          className='w-full h1/3 bg-cover p-8'
+          className='w-full h1/3 bg-cover p-8 md:h-1/2'
           style={{ backgroundImage: 'url($category.img)' }}
         >
           <div className={'text-${category.color} w-1/2'}>
             <h1 className='uppercase font-bold text-3xl'>{category.title}</h1>
             <p className='text-sm my-8'>{category.desc}</p>
-            <button className={'hidden 2xl:block'}>Explore</button>
+            <button
+              className={
+                'hidden 2xl:block bg-${category.color text-red-500 py-2 px-4 rounded}'
+              }
+            >
+              Explore
+            </button>
           </div>
         </Link>
       ))}
