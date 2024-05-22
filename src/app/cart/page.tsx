568008c4bb7d32f6b1f9b18ed7fdf666fb00a795
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-05-21 23:58:55 -0400

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 const CartPage = () => {
   return (
-    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500'>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row'>
       {/* Products Container */}
-      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll'>
+      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll kg:h-full lg:w-23'>
         {/* Single item */}
         <div className='flex items-center justify-between mb-4'>
           <Image src='/temporary/p1.png' alt='' width={100} height={100} />
@@ -48,13 +48,26 @@ const CartPage = () => {
         </div>
       </div>
       {/* Payment Container */}
-      <div className='h-1/2 p-4 bg-fuschia-50 flex flex-col gap-4 justify-center'>
-        <div className='flex'>
+      <div className='h-1/2 p-4 bg-fuschia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3'>
+        <div className='flex justify-between'>
           <span>Subtotal (3 items)</span>
           <span>$81.70</span>
         </div>
+        <div className='flex justify-between'>
+          <span>Service Cost</span>
+          <span>$0.00</span>
+        </div>
+        <div className='flex justify-between'>
+          <span>Delivery Cost</span>
+          <span className='text-green-500'>FREE!</span>
+        </div>
+        <hr className='my-2' />
+        <div className='flex justify-between'>
+          <span>TOTAL(INCL. VAT)</span>
+          <span className='font-bold'>$81.70</span>
+        </div>
         <hr className='' />
-        <button className='bg-red-500 text-white p-3 rounded-md w-1/2'>
+        <button className='bg-red-500 text-white p-3 rounded-md w-1/2 self-end'>
           CheckOut
         </button>
       </div>
