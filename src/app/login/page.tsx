ea86319ede93fa9f78453de5c0230a157e61e277
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-05-25 23:58:24 -0400

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,18 +3,25 @@ import Image from 'next/image'
 
 const LoginPage = () => {
   return (
-    {/* box */}
     <div>
-      <div className="">
+      {/* box */}
+
+      <div className=''>
         {/* Image Container */}
-        <div className="relative">
-          <Image src='' alt='/>
+        <div className='relative'>
+          <Image src='/loginBg.pngd' alt='fill' />
+        </div>
+        {/*Form Container */}
+        <div className=''>
+          <h1>Welcome</h1>
+          <p>Log into your account or create a new one using social buttons</p>
+          <button className=''>
+            <Image src='' alt='' />
+          </button>
         </div>
       </div>
     </div>
-  </div>
   )
-
 }
 
 export default LoginPage
