3fd460ebc61bd81a570a0f0be1adb8ae8279dd49
Gerlin <68314210+gerlinp@users.noreply.github.com>
2024-05-27 23:55:22 -0400

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,15 +9,23 @@ const LoginPage = () => {
       <div className=''>
         {/* Image Container */}
         <div className='relative'>
-          <Image src='/loginBg.pngd' alt='fill' />
+          <Image src='/loginBg.png' alt='fill' />
         </div>
         {/*Form Container */}
         <div className=''>
           <h1>Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className=''>
-            <Image src='' alt='' />
+          <button className='flex gap-4 p-f ring-1 ring-orange-100 rounded-md'>
+            <Image
+              src='/google.png'
+              alt=''
+              width={20}
+              height={20}
+              className='object-contain'
+            />
+            <span>Sign in with Google</span>
           </button>
+          <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md'></button>
         </div>
       </div>
     </div>
